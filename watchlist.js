2fb8f8ad9d7f48f635a98cf94a36d938eb02795c
Aguirre <zenerika@ymail.com>
2018-11-11 20:24:50 -0600

    progress on step 2

--- watchlist.js
+++ watchlist.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     // pulls the watchlist data from local storage
-    const watchlistEl = localStorage.getItem('watchlist')
-    localStorage.getItem('watchlist')
-    console.log('this is my watchlist:', watchlistEl)   
+    var watchlistEl = localStorage.getItem('watchlist')
+    console.log('this is my watchlist:', watchlistEl)
 
     // displays the watchlist movie data into the movie container
     function renderMovies(watchlistEl) {
+        var watchlistJSON = localStorage.getItem('watchlist')
+        var watchlist = JSON.parse(watchlistJSON)
+        if (!watchlist) {
+            watchlist = []
+        }
+
+        watchlist.push(watchlistEl)
+        watchlistJSON = JSON.stringify(watchlist)
+        localStorage.setItem('watchlist', watchlistJSON)
+        console.log('the watchlist is working', watchlist)
+
         var movieHTML = watchlistEl.map(function (currentMovie) {
             return `<div class="card">
             <img class="card-img-top" src=${currentMovie.Poster} alt="Movie Poster">
@@ -17,7 +27,8 @@ document.addEventListener('DOMContentLoaded', function () {
             </div>`
         })
         return movieHTML.join('')
-        console.log('this are my movies:', movieHtml)
     }
-    
+
+    renderMovies(watchlistEl)
+    console.log("my movies are showing", renderMovies)
 })
